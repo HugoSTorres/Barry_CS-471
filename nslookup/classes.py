3bb0ba64468f72375894ce64dc2c7d42e4a7b10d
LeeGiS <lukas_dmx@yahoo.com>
2014-04-27 01:27:01 -0400

    Added comments and modularized the the files

--- nslookup/classes.py
+++ nslookup/classes.py
@@ -6,6 +6,10 @@ import socket, time, getopt, signal, sys
 from termcolor import colored
 from setuptools.command import easy_install
 
+'''
+    The object basically ensures that all the required imports are available on the computer.
+    If they are not, it attempts to install it. If it fails, the program exits
+'''
 class ImportsChecker():
     _MODULES = ('termcolor', 'dns', 'time', 'socket', 'getopt')
     def __init__(self):
@@ -41,6 +45,9 @@ class NSLookup():
             self._printServerInfo()
             self._reverseDNSLookup(domain)
   
+    '''
+        Prints the local DNS server information
+    '''
     def _printServerInfo(self):
         # get nameserver, port and print it
         self._nameserver = self._resolver.nameservers[0]
@@ -49,11 +56,17 @@ class NSLookup():
         print "Address: %s#%s\n\n" % (self._nameserver, self._port)
         print "Non-authoritative answer:" 
         
+    '''
+        Prints the authoritative information for the remote host
+    '''
     def _printAuthoritative(self):
         print "\nAuthoritative answers can be found from:"
         for item in self._authoritative_list:
             print item[0] + " \tinternet address: " + item[1] 
 
+    '''
+        Prints A type records
+    '''
     def _printAInfo(self):
         answer = self._resolver.query(self._domain, 'A')
         
@@ -63,7 +76,10 @@ class NSLookup():
         for item in answer.response.answer[0].items:
             print "Name: \t" + str(answer.response.question[0])[:idx -2]
             print "Address: \t" + item.address
-            
+    
+    '''
+        Prints MX type records
+    '''        
     def _printMXInfo(self):
         answer = self._resolver.query(self._domain, 'MX')
         # let's iterate through A's array and print
@@ -77,7 +93,10 @@ class NSLookup():
             
             # append it to the list for later printing
             self._authoritative_list.append([mx_name, socket.gethostbyname(mx_name)])
-            
+    
+    '''
+        Prints NS type records
+    '''        
     def _printNSInfo(self):
         answer = self._resolver.query(self._domain, 'NS')
         # let's iterate through A's array and print
@@ -91,6 +110,9 @@ class NSLookup():
             # append it to the list for later printing
             self._authoritative_list.append([ns_name, socket.gethostbyname(ns_name)])
     
+    '''
+        Prints SOA type records
+    '''
     def _printSOAInfo(self):
         answer = self._resolver.query(self._domain, 'SOA')
         # let's iterate through A's array and print
@@ -111,20 +133,32 @@ class NSLookup():
         
         self._authoritative_list.append([origin, socket.gethostbyname(origin)])
     
+    '''
+        Prints AAAA type records
+    '''
     def _printAAAAInfo(self):
         answer = self._resolver.query(self._domain, 'AAAA')
         idx = str(answer.response.answer[0]).rindex(' ')
         
         print str(answer.canonical_name) + "\t has AAAA address" + str(answer.response.answer[0])[idx:]
     
+    '''
+        Prints CNAME type records
+    '''
     def _printCNAMEInfo(self):
         answer = self._resolver.query(self._domain, 'CNAME')
         idx = str(answer.response.answer[0]).rindex(' ')
         print str(answer.canonical_name) + "\t canonical name:" + str(answer.response.answer[0])[idx:]
+    '''
+        Since it was hard to find websites that use order records (like SRV or PTR) to test it, I did not filter the results
+        from DNS records for those type and just print them directly
+    '''
     def _printOtherInfo(self):
         answer = self._resolver.query(self._domain, self._type)
         print answer.response.answer[0]
-    
+    '''
+        Prints ANY type records
+    '''
     def _printANYInfo(self):
         self.printSOAInfo()
         self.printMXInfo()
@@ -133,12 +167,21 @@ class NSLookup():
         self.printNSInfo()
         self.printCNAMEInfo()
     
+    '''
+        Looks up the domain name from the IP address
+    '''
     def _reverseDNSLookup(self,ip):
         addr = dns.reversename.from_address(ip)
         print addr 
     
+    '''
+        Sets the port through which to establish the communication
+    '''
     def setPort(self, port):
         self._port = port
+    '''
+        Sets the timeout for a single try
+    '''
     def setTimeout(self, timeout):
         self._timeout
         
@@ -175,83 +218,3 @@ class NSLookup():
         
         # at the end print authoritative response
         self._printAuthoritative()
-
-#printServerInfo()
-
-#printAInfo(domain_name)
-#printMXInfo(domain_name)
-#printNSInfo(domain_name)
-#printSOAInfo(domain_name)
-#printAAAAInfo(domain_name)
-#printANYInfo(domain_name)
-#printOtherInfo(domain_name, 'A')
-#printCNAMEInfo(domain_name)
-# exceptions to handle : NoAnswer, Timeout
-#printAuthoritative()
-#reverseDNSLookup("74.125.21.102")
-
-usage = {'query': False, 'domain' : False, 'timeout' : False, 'port' : False, 'ip' : False}
-def main(argv):
-    global usage
-    try:
-        opts, args = getopt.getopt(argv,"hq:d:t:p:i:",
-                                   ['query=', 'domain=', 'timeout=', 'port=', 'ip='])
-    except getopt.GetoptError:
-        print 'Type in \'python nslookup.py -h\' for help'
-        exit()
-    if not opts:
-        print 'Type in \'python nslookup.py -h\' for help'
-        exit()
-    for opt, arg in opts:
-        if opt == '-h':
-            print '''usage: python nslookup.py [options]
-            Options:
-            
-            -q type, --query=type                          Sets the type of the lookup (for example, A, MX, CNAME,
-                                                           AAAA, ANY, NS, SOA). Default is A
-            -d [domain name], --domain=[domain name]       Domain name (or IP address) for which to perform the NS lookup
-            -t timeout, --timeout=timeout                  Change the initial timeout interval for waiting for a reply to number seconds
-            -p port, --port=port                           Change the default TCP/UDP name server port value. Default is 53
-            -i [IP Adress], --ip=[IP Address]              When chosen, the software will perform the reverse name lookup             
-            -h                                             Shows this help menu
-            '''
-            sys.exit()
-        elif opt in ('-q', '--query'):
-            usage['query'] = arg.upper()
-        elif opt in ('-d', '--domain'):
-            usage['domain'] = arg
-        elif opt in ('-t', '--timeout'):
-            usage['timeout'] = arg
-        elif opt in ('-p', '--port'):
-            usage['port'] = arg
-        elif opt in ('-i', '--ip'):
-            usage['ip'] = arg
-        
-def cleanExit(signum, frm):
-    print colored("Exiting the program", 'red')
-    exit()
-    
-if __name__ == '__main__':
-    main(sys.argv[1:])
-    ch = ImportsChecker()
-
-    signal.signal(signal.SIGINT, cleanExit)
-    if usage['domain']:
-        ns = NSLookup(usage['domain'])
-        
-        # set environment variables
-        if usage['timeout']:
-            ns.setTimeout(usage['timeout'])
-        if usage['port']:
-            ns.setPort(usage['port']) 
-        
-        #execute query
-        if usage['query']:
-            ns.executeType(usage['query'])
-        else:
-            ns.executeType('A')
-    elif usage['ip'] :
-        ns = NSLookup(usage['ip'], True)
-    else:
-        print colored('One of the arguments -d or -i must be supplied', 'red')
-        exit()
