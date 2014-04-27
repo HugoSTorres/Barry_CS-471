14938aedae99057f02f98449ee0ed74d78dc51c9
LeeGiS <lukas_dmx@yahoo.com>
2014-04-26 21:20:44 -0400

    Prints server info and A type resolution

--- nslookup/main.py
+++ nslookup/main.py
@@ -3,6 +3,32 @@ import dns.name
 import dns.query
 import dns.resolver
 
+def printServerInfo():
+    # get nameserver, port and print it
+    nameserver = my_resolver.nameservers[0]
+    port = my_resolver.port
+    print "Server: %s" % nameserver
+    print "Address: %s#%s\n\n" % (nameserver, port)
+    
+def printAInfo(domain):
+    answer = my_resolver.query(domain_name, 'A')
+    
+    # index for substring
+    idx = str(answer.response.question[0]).rindex('IN A')
+    
+    # let's iterate through A's array and print
+    print "Non-authoritative answer:"
+    for item in answer.response.answer[0].items:
+        print "Name: " + str(answer.response.question[0])[:idx -2]
+        print "Address: " + item.address
+        
+def printMXInfo(domain):
+    answers = my_resolver.query('google.com', 'MX')
+    for rdata in answers:
+        print 'Host', rdata.exchange, 'has preference', rdata.preference
 my_resolver = dns.resolver.Resolver()
-nameserver = my_resolver.nameservers
-port = my_resolver.port
+printServerInfo()
+
+domain_name = "www.google.com"
+#printAInfo(domain_name)
+printMXInfo(domain_name)
