c503506c64080228b5dca86a4a685e11080a41b0
LeeGiS <lukas_dmx@yahoo.com>
2014-04-26 22:03:34 -0400

    Finished MX, starting NS

--- nslookup/main.py
+++ nslookup/main.py
@@ -2,6 +2,7 @@ import dns
 import dns.name
 import dns.query
 import dns.resolver
+import socket
 
 def printServerInfo():
     # get nameserver, port and print it
@@ -19,16 +20,37 @@ def printAInfo(domain):
     # let's iterate through A's array and print
     print "Non-authoritative answer:"
     for item in answer.response.answer[0].items:
-        print "Name: " + str(answer.response.question[0])[:idx -2]
-        print "Address: " + item.address
+        print "Name: \t" + str(answer.response.question[0])[:idx -2]
+        print "Address: \t" + item.address
         
 def printMXInfo(domain):
-    answers = my_resolver.query('google.com', 'MX')
-    for rdata in answers:
-        print 'Host', rdata.exchange, 'has preference', rdata.preference
+    answer = my_resolver.query(domain, 'MX')
+    # let's iterate through A's array and print
+    authoritative_list = []
+    
+    # print the non authoritative answer
+    print "Non-authoritative answer:"
+    for data in answer:
+        print answer.canonical_name[0] + '.' + answer.canonical_name[1] + "\t mail exchanger: " + str(data)
+        
+        # extract the hostname of MX name server
+        idx = str(data).index(' ')
+        mx_name = str(data)[idx +1:len(str(data)) - 1]
+        
+        # append it to the list for later printing
+        authoritative_list.append([mx_name, socket.gethostbyname(mx_name)])
+    print "\nAuthoritative answers can be found from:"
+    for item in authoritative_list:
+        print item[0] + " \tinternet address: " + item[1]
+        
+def printNSInfo(domain):
+    answer = my_resolver.query(domain, 'NS')
 my_resolver = dns.resolver.Resolver()
 printServerInfo()
 
-domain_name = "www.google.com"
+domain_name = "google.com"
+print socket.gethostbyname("alt2.aspmx.l.google.com")
 #printAInfo(domain_name)
 printMXInfo(domain_name)
+
+# exceptions to handle : NoAnswer, Timeout
